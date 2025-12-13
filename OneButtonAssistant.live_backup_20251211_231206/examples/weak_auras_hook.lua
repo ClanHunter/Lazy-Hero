22ef6adf28535c2a26d57d549b5f8f2fa7516e01
LazyHeroBot <lazyhero@example.local>
2025-12-13 13:54:10 -0600

    Single commit: shared adapter helpers and bglogger patches

--- OneButtonAssistant.live_backup_20251211_231206/examples/weak_auras_hook.lua
+++
@@ -1,7 +0,0 @@
--- Example WeakAuras hook: function WeakAuras can call to get next action
-function OneButtonAssistant_GetNextAction(unit)
-  if OneButtonAssistantEngine then
-    return OneButtonAssistantEngine:GetNextAction(unit)
-  end
-  return nil
-end
